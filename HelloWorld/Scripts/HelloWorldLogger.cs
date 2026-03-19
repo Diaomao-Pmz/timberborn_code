9e6655aa6be6bc7a5e0f991f6ee565f7f23f36f2
Diaomao <pmzomo030831@gmail.com>
2026-03-18 19:35:56 -0700

    11

---
+++ HelloWorld/Scripts/HelloWorldLogger.cs
@@ -0,0 +1,13 @@
+﻿using Timberborn.ModManagerScene;
+using UnityEngine;
+
+namespace Mods.HelloWorld.Scripts {
+  internal class HelloWorldLogger : IModStarter {
+
+    public void StartMod(IModEnvironment modEnvironment) {
+      var playerLogPath = Application.persistentDataPath + "/Player.log";
+      Debug.Log("Hello World, but in the Player.log file at: " + playerLogPath);
+    }
+
+  }
+}
